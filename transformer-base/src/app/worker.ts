1f004bf71ffe373fc360368858688ceb78ce62df
marrej <marcus.revaj@gmail.com>
2025-04-05 21:49:14 +0000

    # Use text generation

--- transformer-base/src/app/worker.ts
+++ transformer-base/src/app/worker.ts
@@ -1,13 +1,21 @@
 import { pipeline } from "@huggingface/transformers";
 
+// Supported tasks https://huggingface.co/docs/transformers.js/en/index#natural-language-processing
 // Use the Singleton pattern to enable lazy construction of the pipeline.
 class PipelineSingleton {
-    static task = 'text-classification';
-    static model = 'Xenova/distilbert-base-uncased-finetuned-sst-2-english';
+    // Classifier
+    // static task = 'text-classification';
+    // static model = 'Xenova/distilbert-base-uncased-finetuned-sst-2-english';
+
+    static task = 'text-generation';
+    // text-generation https://huggingface.co/models?pipeline_tag=text-generation&library=transformers.js
+    static model = 'HuggingFaceTB/SmolLM2-1.7B-Instruct';
     static instance = null;
 
+    // NOTE: Requires a `model_quantized.onnx` file to be present otherwie it won't run
     static async getInstance(progress_callback = null) {
-        this.instance ??= pipeline(this.task, this.model, { progress_callback });
+        // https://github.com/huggingface/transformers.js/blob/main/src/pipelines.js#L3400
+        this.instance ??= pipeline(this.task, this.model, { progress_callback, model_file_name: 'model' });
         return this.instance;
     }
 }
@@ -16,15 +24,15 @@ class PipelineSingleton {
 self.addEventListener('message', async (event) => {
     // Retrieve the classification pipeline. When called for the first time,
     // this will load the pipeline and save it for future use.
-    const classifier = await PipelineSingleton.getInstance((x: any) => {
+    const generation = await PipelineSingleton.getInstance((x: any) => {
         // We also add a progress callback to the pipeline so that we can
         // track model loading.
         self.postMessage(x);
     });
-    if (!classifier) {return;}
+    if (!generation) {return;}
 
-    // Actually perform the classification
-    const output = await classifier(event.data.text);
+    // Actually perform the generation
+    const output = await generation(event.data.text);
 
     // Send the output back to the main thread
     self.postMessage({
